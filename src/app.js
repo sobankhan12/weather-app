2c635825f556e1e67802c291b769a8724a8b2dcc
sobankhan <sobankhan252@gmail.com>
2021-04-13 13:25:47 +0500

    some changes in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const geoCode=require("./utils/geocode")
 const forecast=require("./utils/forecast")
 // console.log(address);
+const port=process.env.PORT || 3000
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
