655bf75e0e27326aa640279185067cfa576d6271
sobankhan <sobankhan252@gmail.com>
2021-04-13 14:32:05 +0500

    aded new features

--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,23 @@
-console.log('Client side javascript file is loaded!')
-const weatherForm=document.querySelector("form");
-const search=document.querySelector("input")
-const messageOne=document.getElementById("message1")
-const messageTwo=document.getElementById("message2")
-const inputText=document.getElementById("inputValue")
-weatherForm.addEventListener("submit",(e)=>{
-    const location=search.value;
-    // inputText.value=""
-    // search.value=""
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
-    // search.value=" "
-    // console.log(location);
-    fetch(`/weather?address=${location}`).then((res)=>{
-        res.json().then((data)=>{
-            if(data.error){
-                console.log(data.error);
-                messageOne.textContent=data.error
-                messageTwo.textContent=""
-            }else{
-                messageOne.textContent=data.location
-                messageTwo.textContent=data.forecast
-                console.log(data.location);
-                console.log(data.forecast);
+
+    const location = search.value
+
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+    fetch('/weather?address=' + location).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
             }
         })
     })
